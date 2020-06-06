6ca0cd62678318ac048ab3f76986424e7bbe9818
superatom <crazycommander111@gmail.com>
2020-06-06 11:00:29 +0700

    Initial Commit

---
+++ ruler_example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ruler_example
+//
+//  Created by Le Xuan Khanh on 6/6/20.
+//  Copyright © 2020 Le Xuan Khanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ruler_exampleTests/ruler_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ruler_exampleTests.swift
+//  ruler_exampleTests
+//
+//  Created by Le Xuan Khanh on 6/6/20.
+//  Copyright © 2020 Le Xuan Khanh. All rights reserved.
+//
+
+import XCTest
+@testable import ruler_example
+
+class ruler_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
